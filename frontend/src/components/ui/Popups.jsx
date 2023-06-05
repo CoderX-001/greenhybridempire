687b3ae798cade104f0287a838cca9df28081d6a
coderX_001 <emmanuelkhila200@gmail.com>
2023-06-05 09:42:48 +0100

    new changes #9

--- frontend/src/components/ui/Popups.jsx
+++ frontend/src/components/ui/Popups.jsx
@@ -8,65 +8,25 @@ import { BiSearch } from "react-icons/bi";
 import { AppContext } from "../../contexts/AppContext";
 
 export const SearchPopup = () => {
-  const {searchState} = useContext(AppContext)
+  const { searchState } = useContext(AppContext);
   const [screenHeight, setScreenHeight] = useState(getAvailableScreenHeight());
-  window.addEventListener("resize", () => setScreenHeight(getAvailableScreenHeight()));
+  window.addEventListener("resize", () =>
+    setScreenHeight(getAvailableScreenHeight())
+  );
 
-  const [inputValue, setInputValue] = useState("")
-  const { searchText, setSearchText, searchActive, setSearchActive } = searchState
+  const [inputValue, setInputValue] = useState("");
+  const { searchText, setSearchText, searchActive, setSearchActive } =
+    searchState;
 
   return (
     <div
-      className={`search-container ${searchActive ? "active" : ""}`}
-      style={{height: searchActive ? screenHeight : 0 + "px"}}
+      className={`w-full fixed z-[9999] top-0 left-0 backdrop-blur ${searchActive ? "py-6 px-4 overflow-auto" : "p-0 overflow-hidden"}`}
+      style={{
+        height: searchActive ? screenHeight + "px" : 0,
+        background: "rgba(255, 255, 255, 0.2)",
+      }}
     >
-      <div>
-        <div className="flex items-center justify-end relative">
-          <button
-            onClick={() => {
-              setInputValue("");
-              setSearchText("");
-              setSearchActive(false);
-            }}
-            className="p-1 rounded-full active:bg-gray-200"
-          >
-            <IconContext.Provider value={{ className: "text-2xl lg:text-3xl" }}>
-              <FaTimes />
-            </IconContext.Provider>
-          </button>
-        </div>
-
-        <div className="mt-6">
-          <form className="relative" onSubmit={(e) => e.preventDefault()}>
-            <PrimaryInput
-              type="text"
-              name="search"
-              placeholder="What are you looking for?"
-              padding="py-3 pl-4 pr-16"
-              onChange={(e) => setInputValue(e.target.value)}
-              value={inputValue}
-            />
-            <ButtonWithIcon
-              icon={<BiSearch />}
-              iconSize="text-xl"
-              backgroundColor="bg-primary"
-              textColor="text-white"
-              padding="px-4 py-3"
-              borderRadius="rounded-lg"
-              miscStyle="absolute top-1/2 -translate-y-1/2 right-1"
-              func={() => setSearchText(inputValue)}
-            />
-          </form>
-        </div>
-
-        <div className="mt-6 relative">
-          {searchText !== "" ? (
-            <p className="text-2xl text-center text-secondary">
-              Search results for &quot;{searchText}&quot;
-            </p>
-          ) : null}
-        </div>
-      </div>
+      <h1>Hello</h1>
     </div>
   );
 };
