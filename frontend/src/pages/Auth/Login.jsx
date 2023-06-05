3d7c3ced6488ab91b03252150fff75985c439938
coderX_001 <emmanuelkhila200@gmail.com>
2023-06-05 18:41:48 +0100

    new changes - fresh start

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -52,7 +52,7 @@ const Login = ({
       >
         <div className="relative">
           {/* form */}
-          <div className="relative mt-16">
+          <div className="relative mt-16 pb-8">
             <div className="w-fit mx-auto mb-6">
               <h1 className="text-2xl text-center text-primary font-semibold">
                 Hi! Welcome back
@@ -62,8 +62,8 @@ const Login = ({
 
             <ButtonWithIcon
               buttonStyle={
-                `flex-row-reverse w-fit mx-auto mb-4 py-2 px-2 rounded-md ${isDark ? "text-primary-gray": "text-black"}
-                shadow-md border-[1px] ${isDark ? "border-primary-gray" : "border-secondary-gray"}`
+                `flex-row-reverse w-fit mx-auto mb-4 py-2 px-3 rounded-md ${isDark ? "text-primary-gray": "text-black"}
+                shadow-md border-[1px] border-primary`
               }
               link="https://www.developer.google.com/"
               text="Log in with Google"
@@ -74,7 +74,7 @@ const Login = ({
 
             <div className="w-3/4 mx-auto relative mb-12">
               <div className="w-full h-[1px] bg-gray-400 absolute top-1/2 -translate-y-1/2"></div>
-              <p className={`relative w-fit px-1 rounded-full mx-auto z-[2] text-center ${isDark ? "bg-[#121212] text-primary-gray" : "bg-white text-secondary-gray"}`}>
+              <p className={`relative w-fit px-2 py-1 rounded-full mx-auto z-[2] text-center ${isDark ? "bg-[#121212] text-primary-gray" : "bg-white text-secondary-gray"}`}>
                 or
               </p>
             </div>
